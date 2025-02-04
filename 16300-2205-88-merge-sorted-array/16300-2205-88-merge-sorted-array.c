c24fe70b01ff8e7512ca981564b1e2630f6efa4c
Gopi <gopiyadav4788@gmail.com>
2025-02-04 22:15:25 +0530

    16300-2205-88-merge-sorted-array.c

--- 16300-2205-88-merge-sorted-array/16300-2205-88-merge-sorted-array.c
+++ 16300-2205-88-merge-sorted-array/16300-2205-88-merge-sorted-array.c
@@ -1,10 +1,13 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
     for(int i=0; i<n; i++){
+        // inserting the second array elements in the first array
         nums1[m+i]=nums2[i];
     }
+    // sorting the array (Using Bubble sort)
     for(int i=0; i<m+n-1; i++){
         for(int j=1; j<m+n-i; j++){
             if(nums1[j-1]>nums1[j]){
+                // swapping the elements
                 int temp=nums1[j-1];
                 nums1[j-1]=nums1[j];
                 nums1[j]=temp;
