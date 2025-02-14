7cb5ce4941cf7480c4aeef0457f999eefb0fe072
Gopi <gopiyadav4788@gmail.com>
2025-02-14 06:41:58 +0530

    Time: 18 ms, Memory: 9.1 MB - LeetHub

---
+++ 3003-191-941-valid-mountain-array/3003-191-941-valid-mountain-array.c
@@ -0,0 +1,52 @@
+
+
+bool validMountainArray(int* arr, int arrSize){
+    // checking if the size of the array is >= 3 or not
+	if(arrSize<=2){
+		return false;
+	}
+    // finding the index of maximum value in the array
+	int maxidx=0;
+	for(int i=1; i<arrSize; i++){
+		if(arr[maxidx]<arr[i]){
+			maxidx=i;
+		}
+	}
+    // Checking if maxidx is 0 or arrSize-1
+    if (maxidx==(arrSize-1) || maxidx==0){
+        return false;
+    }
+	int tempmax=arr[maxidx];
+	int cnt=0;
+	// dividing array into two parts onepart before the maximum element and another part after the maximum element
+
+    // Checking is firstpart is strictly decreasing from the (maxidx-1 to 0)
+	for(int i=maxidx-1; i>=0; i--){
+		if(arr[i]>=tempmax){
+			cnt++;
+			break;
+		}
+		tempmax=arr[i];
+	}
+    // Checking second part whether it is strictly decresing or not from (maxidx+1 to arrSize-1) if and only if first part is strictly decreasing from (maxidx-1 to 0)
+	if(cnt==0){
+	for(int i=maxidx+1; i<arrSize; i++ ){
+		if(arr[i]>=arr[maxidx]){
+			cnt++;
+			break;
+		}
+		arr[maxidx]=arr[i];
+	}
+    
+    }
+    if(cnt==0){
+	return true;
+    }else{
+	return false;
+}
+
+
+
+
+
+}
