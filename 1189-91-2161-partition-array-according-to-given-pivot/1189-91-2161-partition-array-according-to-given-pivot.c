3b264f60e0b713a71ca4384fdb5f6a80de9457b7
Gopi <gopiyadav4788@gmail.com>
2025-03-03 07:55:42 +0530

    Time: 8 ms, Memory: 98.4 MB - LeetHub

---
+++ 1189-91-2161-partition-array-according-to-given-pivot/1189-91-2161-partition-array-according-to-given-pivot.c
@@ -0,0 +1,36 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
+    *returnSize = numsSize;
+    int * Arr=(int *)calloc(numsSize,sizeof(int));
+    int lcnt=0;
+    int hcnt=0;
+    int ecnt=0;
+    for(int i=0; i<numsSize; i++){
+        if(nums[i]>pivot){
+            hcnt++;
+        }else if(nums[i]<pivot){
+            lcnt++;
+        }else{
+            ecnt++;
+        }
+    }
+    int lidx=0;
+    int eidx=lcnt;
+    int hidx=lcnt+ecnt;
+    for(int i=eidx ; i<eidx+ecnt; i++){
+        Arr[i]=pivot;
+    }
+    for(int i=0; i<numsSize; i++){
+        if(nums[i]<pivot){
+            Arr[lidx]=nums[i];
+            lidx++;
+        }else if(nums[i]>pivot){
+            Arr[hidx]=nums[i];
+            hidx++;
+        }
+    }
+    return Arr;
+
+}
