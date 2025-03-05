571a8ae1b341627f5e56e9cd68f881bf8a4ee7d5
Gopi <gopiyadav4788@gmail.com>
2025-03-05 07:27:11 +0530

    0337-29-2579-count-total-number-of-colored-cells.c

--- 0337-29-2579-count-total-number-of-colored-cells/0337-29-2579-count-total-number-of-colored-cells.c
+++ 0337-29-2579-count-total-number-of-colored-cells/0337-29-2579-count-total-number-of-colored-cells.c
@@ -1,3 +1,6 @@
 long long coloredCells(int n) {
+    // sum of first n odd natural numbers 
+    // sum of first n-1 odd natural numbers
+    // ex : if n=5 , ans = 1 3 5 7 9 7 5 3 1
     return pow(n,2)+pow(n-1,2);
 }
