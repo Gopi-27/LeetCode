e38f794433d6cfcf0ecca414d1ae45c24ed83d76
Gopi <gopiyadav4788@gmail.com>
2025-03-19 06:47:59 +0530

    0184-9-3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.c

--- 0184-9-3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/0184-9-3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.c
+++ 0184-9-3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/0184-9-3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.c
@@ -1,17 +1,15 @@
 int minOperations(int* nums, int n) {
-    int optr = -1;
     int cnt = 0;
     for(int i = 0; i < n; i++ ){
         if(nums[i] == 0){
             if(i + 2 >= n)return -1;
-            optr = i;
             cnt++;
             if(nums[i + 1] == 0)nums[i + 1] = 1;
             else nums[i + 1] = 0;
             if(nums[i + 2] == 0)nums[i + 2] = 1;
             else nums[i + 2] = 0;
             
-        }else optr = i;
+        }
     }
     return cnt;
     
