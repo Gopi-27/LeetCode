6af50e3764a806b0d090fc48d978a06c8d823a4e
Gopi <gopiyadav4788@gmail.com>
2025-04-08 20:50:46 +0530

    Time: 0 ms, Memory: 8.4 MB - LeetHub

---
+++ 1449-73-1991-find-the-middle-index-in-array/1449-73-1991-find-the-middle-index-in-array.c
@@ -0,0 +1,13 @@
+int findMiddleIndex(int* nums, int numsSize) {
+    int lsum = 0; 
+    int rsum = 0;
+    for(int i = 1; i < numsSize; i++)rsum += nums[i];
+    int left = -1;
+    int right = 1;
+    while(lsum != rsum  && right < numsSize){
+        lsum += nums[++left];
+        rsum -= nums[right++];
+    }
+    if(lsum == rsum)return left + 1;
+    return -1;
+}
