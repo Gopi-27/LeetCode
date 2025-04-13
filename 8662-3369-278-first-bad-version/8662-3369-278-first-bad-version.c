129495d48732a2e815b9531205b1558f59087174
Gopi <gopiyadav4788@gmail.com>
2025-04-13 14:17:27 +0530

    Time: 2 ms, Memory: 7.7 MB - LeetHub

---
+++ 8662-3369-278-first-bad-version/8662-3369-278-first-bad-version.c
@@ -0,0 +1,16 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    long long int low = 1;
+    long long int high = n;
+    while(low <= high){
+        long long int mid = (low + high) >> 1;
+        if(isBadVersion(mid)){
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return low;
+}
