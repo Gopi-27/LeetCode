20600ddd680777aeaba45113e722110f9860d69d
Gopi <gopiyadav4788@gmail.com>
2025-04-22 07:51:56 +0530

    Time: 0 ms, Memory: 10.6 MB - LeetHub

---
+++ 0150-3-3248-snake-in-matrix/0150-3-3248-snake-in-matrix.c
@@ -0,0 +1,11 @@
+int finalPositionOfSnake(int n, char** commands, int commandsSize) {
+    int row = 0;
+    int col = 0;
+    for(int i = 0; i < commandsSize; i++){
+        if(strcmp(commands[i],"UP") == 0)row--;
+        else if(strcmp(commands[i],"DOWN") == 0)row++;
+        else if(strcmp(commands[i],"LEFT") == 0)col--;
+        else col++;
+    }
+    return (row * n) + col;
+}
