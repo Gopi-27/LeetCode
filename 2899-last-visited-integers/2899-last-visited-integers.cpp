b1d1471dcee8a6e5810cc795dcf36bd219d4cb53
Gopi <gopiyadav4788@gmail.com>
2025-06-07 11:53:44 +0530

    Time: 0 ms (100%), Space: 26 MB (41.18%) - LeetHub

---
+++ 2899-last-visited-integers/2899-last-visited-integers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> lastVisitedIntegers(vector<int>& nums) {
+        vector<int>seen;
+        vector<int>ans;
+        int k = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0){
+                seen.insert(seen.begin(),nums[i]);
+                k = 0;
+            }else{
+                k++;
+                if(k <= seen.size()){
+                    ans.push_back(seen[k - 1]);
+                }else{
+                    ans.push_back(-1);
+                }
+            }
+        }
+        return ans;
+    }
+};
