c68c50a3ff5a305fe14197cc2c9a7564ed95e418
Gopi <gopiyadav4788@gmail.com>
2025-08-03 06:49:36 +0530

    Time: 733 ms (81.41%), Space: 0B (100%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,12 @@
+/* Write your PL/SQL query statement below */
+
+
+SELECT 
+u.name,COALESCE(SUM(r.distance),0) AS travelled_distance
+FROM
+users u
+LEFT JOIN
+rides r
+ON u.id = r.user_id
+GROUP BY u.id,u.name
+ORDER BY travelled_distance DESC NULLS LAST,u.name;
