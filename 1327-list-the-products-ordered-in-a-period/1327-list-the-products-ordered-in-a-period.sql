a715050acade87a5c3389f4fdd70052bf5cb41b6
Gopi <gopiyadav4788@gmail.com>
2025-08-03 06:27:56 +0530

    Time: 709 ms (69.72%), Space: 0B (100%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,12 @@
+/* Write your PL/SQL query statement below */
+SELECT 
+p.product_name AS product_name,
+SUM(o.unit) AS unit
+FROM
+products p
+JOIN
+orders o
+ON p.product_id = o.product_id
+WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY product_name
+HAVING SUM(o.unit) >= 100;
