c8b4e7224e16f9419f67051c5d78f24cd40d12ef
Gopi <gopiyadav4788@gmail.com>
2025-08-09 19:54:29 +0530

    Time: 20 ms (88.21%), Space: 26.2 MB (36.61%) - LeetHub

---
+++ 1447-simplified-fractions/1447-simplified-fractions.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<string> simplifiedFractions(int n) {
+        vector<string>Ans;
+        for(int i = 1; i < n; i++){
+            for(int j = i + 1; j <= n; j++){
+                if(gcd(i,j) == 1)Ans.push_back(to_string(i) + "/" + to_string(j));
+            }
+        }
+        return Ans;
+    }
+};
