c79ae5a12aca2ccd187ac56082a1606c6eaafcbe
Gopi <gopiyadav4788@gmail.com>
2025-10-15 14:02:43 +0530

    Time: 220 ms (76.32%), Space: 173.5 MB (89.47%) - LeetHub

--- 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,47 +1,19 @@
 class Solution {
 public:
-bool hasIncreasingSubarrays(vector<int>& nums, int k) {
-        if(k <= 1)return true;
-        int n = nums.size();
-
-        int cnt = 0;
-        int prev_last = -2000;
-        int cur_first = nums[0];
-        int i = 1;
-        while( i < n){
-            int curlen = 1;
-            while(curlen < k && i < n && nums[i - 1] < nums[i]){
-                curlen++;
-                i++;
-            }
-            if(curlen == k){
-                prev_last = nums[i - 1];
-                cnt++;
-                if(cnt == 2)return true;
-            }else{
-                if(cnt & prev_last < cur_first){
-                        prev_last = nums[i - 1];
-                }else{
-                    cnt = 0;
-                }
-            }
-            cur_first = nums[i];
-            i++;            
-        }
-        return false;
-    }
     int maxIncreasingSubarrays(vector<int>& nums) {
+        int cur = 1;
+        int prev = 0;
+        int ans = 0;
         int n = nums.size();
-        int low = 1;
-        int high = n/2;
-        while(low <= high){
-            int mid = (low + high) >> 1;
-            if(hasIncreasingSubarrays(nums,mid)){
-                low = mid + 1;
+        for(int i = 1; i < n; i++){
+            if(nums[i - 1] < nums[i]){
+                cur++;
             }else{
-                high = mid - 1;
+                prev = cur;
+                cur = 1;
             }
+            ans = max(ans,max(cur/2,min(prev,cur)));
         }
-        return low - 1;
+        return ans;
     }
 };
