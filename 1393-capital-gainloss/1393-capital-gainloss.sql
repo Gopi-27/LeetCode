a5c8202f8f29094d50ca1cfaa55c0591eee41c8c
Gopi <gopiyadav4788@gmail.com>
2025-10-18 20:21:33 +0530

    Time: 430 ms (100%), Space: 0B (100%) - LeetHub

---
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+
+
+SELECT stock_name,SUM(CASE WHEN operation = 'Sell' THEN price ELSE 0 END)
+-
+SUM(CASE WHEN operation = 'Buy' THEN price ELSE 0 END) AS capital_gain_loss
+FROM stocks
+GROUP BY stock_name;
