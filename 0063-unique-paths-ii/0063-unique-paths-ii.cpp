f728a9048a90a554ac96f4d49c3f67f51e99fa4c
Gopi <gopiyadav4788@gmail.com>
2025-11-11 08:01:21 +0530

    Time: 2 ms (14.06%), Space: 11.8 MB (71.8%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int rec(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>& Dp){
-        if(i < 0 || j < 0)return 0;
-        if(obstacleGrid[i][j])return 0;
-        if(i == 0 && j == 0)return 1;
-
-        if(Dp[i][j] != -1)return Dp[i][j];
-
-        int ans = rec(i,j - 1,obstacleGrid,Dp);
-        ans += rec(i - 1,j,obstacleGrid,Dp);
-        return Dp[i][j] = ans;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        vector<vector<int>>Dp(n,vector<int>(m,-1));
-        return rec(n - 1,obstacleGrid[0].size() - 1,obstacleGrid,Dp);
+        vector<vector<int>>Dp(n,vector<int>(m));
+        // tabulation
+        Dp[0][0] = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(obstacleGrid[i][j])Dp[i][j] = 0;
+                else{
+                    if(i > 0)Dp[i][j] = Dp[i - 1][j];
+                    if(j > 0)Dp[i][j] += Dp[i][j - 1];
+                }
+            }
+        }
+        return Dp[n - 1][m - 1];
     }
 };
