5e1ba2717222c4d7414c928ac7834b0e6bb18cf8
Gopi <gopiyadav4788@gmail.com>
2025-11-11 17:05:32 +0530

    Time: 0 ms (100%), Space: 16.4 MB (13.67%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int rec(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& Dp){
-        if(i < 0 || j < 0)return INT_MAX;
-        if(i == 0 && j == 0)return grid[i][j];
-        if(Dp[i][j] != -1)return Dp[i][j];
-        int ans = rec(i,j - 1,grid,Dp);
-        ans = min(ans,rec(i - 1,j,grid,Dp));
-
-        return Dp[i][j] = grid[i][j] + ans;
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>>Dp(n,vector<int>(m,-1));
-        return rec(n - 1, m - 1,grid,Dp);
+        // Tabulation
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 && j == 0)Dp[i][j] = grid[i][j];
+                else{
+                    int mini = INT_MAX;
+                    if(i > 0)mini = min(mini,Dp[i - 1][j]);
+                    if(j > 0)mini = min(mini,Dp[i][j - 1]);
+                    Dp[i][j] = mini + grid[i][j];
+                }
+            }
+        }
+        return Dp[n - 1][m - 1];
     }
 };
