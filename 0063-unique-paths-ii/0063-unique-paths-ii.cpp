b944abbcf44f11eaac4682a0b2537dcc17bd6694
Gopi <gopiyadav4788@gmail.com>
2025-11-11 08:04:48 +0530

    Time: 0 ms (100%), Space: 11.6 MB (85.07%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,18 +3,20 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        vector<vector<int>>Dp(n,vector<int>(m));
-        // tabulation
-        Dp[0][0] = 1;
+        // space optimization
+        vector<int>prev(m);
+        prev[0] = 1;
         for(int i = 0; i < n; i++){
+            vector<int>curr(m);
             for(int j = 0; j < m; j++){
-                if(obstacleGrid[i][j])Dp[i][j] = 0;
+                if(obstacleGrid[i][j])curr[j] = 0;
                 else{
-                    if(i > 0)Dp[i][j] = Dp[i - 1][j];
-                    if(j > 0)Dp[i][j] += Dp[i][j - 1];
+                    curr[j] = prev[j];
+                    if(j > 0)curr[j] += curr[j - 1];; 
                 }
             }
+            prev = curr;
         }
-        return Dp[n - 1][m - 1];
+        return prev[m - 1];
     }
 };
