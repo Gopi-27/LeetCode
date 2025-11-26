20dfdb9b1a5d9a5e88a9f4334d9535e91ba58b87
Gopi <gopiyadav4788@gmail.com>
2025-11-26 07:21:37 +0530

    Time: 238 ms (70.14%), Space: 119.6 MB (91.23%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -16,17 +16,38 @@ public:
     int numberOfPaths(vector<vector<int>>& grid, int k) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<vector<int>>>Dp(n,vector<vector<int>>(m,vector<int>(k)));
-        // Tabulation
-        for(int s = 0; s < k; s++)if((s + grid[0][0]) % k == 0)Dp[0][0][s] = 1;
-        for(int i = 0; i < n; i++){
+        // vector<vector<vector<int>>>Dp(n,vector<vector<int>>(m,vector<int>(k)));
+        // // Tabulation
+        // for(int s = 0; s < k; s++)if((s + grid[0][0]) % k == 0)Dp[0][0][s] = 1;
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         for(int s = 0; s < k; s++){
+        //             if(i > 0)Dp[i][j][s] = (Dp[i][j][s] + Dp[i - 1][j][(s + grid[i][j]) % k]) % 1000000007;
+        //             if(j > 0)Dp[i][j][s] = (Dp[i][j][s] + Dp[i][j - 1][(s + grid[i][j]) % k]) % 1000000007;
+        //         }
+        //     }
+        // }
+        // return Dp[n - 1][m - 1][0];
+
+        // Space Optimization
+        vector<vector<int>>prev(m,vector<int>(k));
+        for(int s = 0; s < k; s++)if((s + grid[0][0]) % k == 0)prev[0][s] = 1;
+
+        for(int j = 1; j < m; j++)
+            for(int s = 0; s < k; s++)
+                prev[j][s] += prev[j - 1][(s + grid[0][j]) % k];
+            
+        for(int i = 1; i < n; i++){
+            vector<vector<int>>curr(m,vector<int>(k));
             for(int j = 0; j < m; j++){
                 for(int s = 0; s < k; s++){
-                    if(i > 0)Dp[i][j][s] = (Dp[i][j][s] + Dp[i - 1][j][(s + grid[i][j]) % k]) % 1000000007;
-                    if(j > 0)Dp[i][j][s] = (Dp[i][j][s] + Dp[i][j - 1][(s + grid[i][j]) % k]) % 1000000007;
+                    if(i > 0)curr[j][s] = (curr[j][s] + prev[j][(s + grid[i][j]) % k]) % 1000000007;
+                    if(j > 0)curr[j][s] = (curr[j][s] + curr[j - 1][(s + grid[i][j]) % k]) % 1000000007;
+
                 }
             }
+            prev = curr;
         }
-        return Dp[n - 1][m - 1][0];
+        return prev[m - 1][0];
     }
 };
