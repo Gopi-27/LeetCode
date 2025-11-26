a45e3b8582fa6bfe2c9d6b133165cfd385173534
Gopi <gopiyadav4788@gmail.com>
2025-11-26 06:39:45 +0530

    Time: 251 ms (65.64%), Space: 132 MB (83.17%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -16,7 +16,17 @@ public:
     int numberOfPaths(vector<vector<int>>& grid, int k) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<vector<int>>>Dp(n,vector<vector<int>>(m,vector<int>(k,-1)));
-        return rec(n - 1,m - 1,0,k,grid,Dp);
+        vector<vector<vector<int>>>Dp(n,vector<vector<int>>(m,vector<int>(k)));
+        // Tabulation
+        for(int s = 0; s < k; s++)if((s + grid[0][0]) % k == 0)Dp[0][0][s] = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                for(int s = 0; s < k; s++){
+                    if(i > 0)Dp[i][j][s] = (Dp[i][j][s] + Dp[i - 1][j][(s + grid[i][j]) % k]) % 1000000007;
+                    if(j > 0)Dp[i][j][s] = (Dp[i][j][s] + Dp[i][j - 1][(s + grid[i][j]) % k]) % 1000000007;
+                }
+            }
+        }
+        return Dp[n - 1][m - 1][0];
     }
 };
