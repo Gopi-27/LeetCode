1ab6b9f2267cca9d2673e160ba6b3b88a854aa17
Gopi <gopiyadav4788@gmail.com>
2026-01-23 06:45:04 +0530

    Time: 7 ms (61.94%), Space: 13.2 MB (58.43%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>>Dp(n + 1,vector<int>(m + 1));
+        for(int j = 0; j <= m; j++)Dp[0][j] = j;
+        for(int i = 0; i <= n; i++)Dp[i][0] = i;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(word1[i - 1] == word2[j - 1])Dp[i][j] = Dp[i - 1][j - 1];
+                else Dp[i][j] = 1 + min(Dp[i - 1][j - 1],min(Dp[i][j - 1],Dp[i - 1][j]));
+            }
+        }
+        return Dp[n][m];
+    }
+};
