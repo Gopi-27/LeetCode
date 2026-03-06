ee41de4e62c54e50ad0ea89350f4967c7e5ae7c8
Gopi <gopiyadav4788@gmail.com>
2026-03-06 17:33:45 +0530

    Time: 585 ms (28.62%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+
+
+SELECT w.id
+FROM weather w
+WHERE w.temperature > (
+                        SELECT t.temperature
+                        FROM weather t 
+                        WHERE t.recorddate = w.recorddate - 1;
+                    )
