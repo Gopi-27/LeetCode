c8057670ba4287ca307df4a55c3bb988a63f3e45
Gopi <gopiyadav4788@gmail.com>
2026-04-10 10:07:51 +0530

    Time: 0 ms (100%), Space: 11.5 MB (23.52%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        int ans = 0;
+        vector<int>diff(n);
+        for(int i = 0; i < n; i++){
+            ans += costs[i][0];
+            diff[i] = costs[i][1] - costs[i][0];
+        }
+        sort(diff.begin(),diff.end());
+        for(int i = 0; i < n/2; i++){
+            ans += diff[i];
+        }
+        return ans;
+    }
+};
