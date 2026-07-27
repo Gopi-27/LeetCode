ce912dbf21da189c682f1f0f26eeca5fb0ac376d
Gopi <gopiyadav4788@gmail.com>
2026-07-27 19:40:35 +0530

    Time: 0 ms (100%), Space: 172.6 MB (13.47%) - LeetHub

---
+++ 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long findMaximumScore(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = 0;
+        int maxi = nums[0];
+        for(int i = 0; i < n - 1; i++){
+            maxi = max(maxi,nums[i]);
+            ans += maxi;
+        }
+        return ans;
+    }
+};
